1759660f690d94063715d916774cce279157ad86
Darlan Filho <nalrad.filho@gmail.com>
2022-03-19 17:47:49 -0300

    Adiciona lógica de favoritos aos cards, clique em imagem e estilização

--- src/components/CardPersonagem/CardPersonagem.jsx
+++ src/components/CardPersonagem/CardPersonagem.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { makeStyles } from '@mui/styles'
+import { FavoritoOffIcon } from 'icons/FavoritoOffIcon'
+import { useFavoritosControl } from 'components/ProviderFavoritos/ProviderFavoritosComponent'
+import { FavoritoOnIcon } from 'icons/FavoritoOnIcon'
 
 const useClasses = makeStyles({
-  rootCard: { display: 'flex', flexDirection: 'column', height: 200 },
-  imagemPersonagem: { width: '100%', height: '77%' },
-  stripe: { width: '100%', backgroundColor: '#d03816', height: '3px' },
+  rootCard: { display: 'flex', flexDirection: 'column', height: 250 },
+  imagemPersonagem: { width: '100%', height: '77%', cursor: 'pointer', '&:hover': { filter: 'brightness(0.75)' } },
+  stripe: { width: '100%', backgroundColor: '#FF0000', height: '5px' },
   bottomCard: {
     width: '100%',
     maxWidth: '100%',
@@ -21,20 +24,38 @@ const useClasses = makeStyles({
     marginRight: 7,
   },
   fonteNome: { fontSize: '1.1rem', fontWeight: 500 },
-  favorito: { display: 'flex', alignItems: 'center', justifyContent: 'center', marginLeft: 'auto' },
+  favorito: { display: 'flex', alignItems: 'center', justifyContent: 'center', marginLeft: 'auto', cursor: 'pointer' },
+  buttonInvisible: { backgroundColor: 'transparent', border: 'none', cursor: 'pointer' },
 })
 
 export const CardPersonagem = ({ instancia }) => {
   const classes = useClasses()
+  const favoritosControl = useFavoritosControl()
+  const [ehFavorito, setEhFavorito] = React.useState(favoritosControl.ehFavorito(instancia.id))
+  const onClickImagem = React.useCallback(() => {
+    console.log(instancia)
+  }, [])
+  const handleClickFavorito = React.useCallback(() => {
+    if (ehFavorito) favoritosControl.removerFavorito(instancia.id)
+    else {
+      const favoritoAdicionado = favoritosControl.addFavorito(instancia)
+      if (!favoritoAdicionado) return console.warn('Quantidade de favoritos atingiu o limite')
+    }
+    setEhFavorito(!ehFavorito)
+  }, [ehFavorito])
   return (
     <div className={classes.rootCard}>
-      <img src={instancia.imageURL} className={classes.imagemPersonagem} />
+      <img src={instancia.imageURL} className={classes.imagemPersonagem} onClick={onClickImagem} alt="" />
       <div className={classes.stripe} />
       <div className={classes.bottomCard}>
         <div className={classes.nomePersonagem}>
           <span className={classes.fonteNome}>{instancia.name}</span>
         </div>
-        <div className={classes.favorito}>Fav</div>
+        <div className={classes.favorito}>
+          <button className={classes.buttonInvisible} onClick={handleClickFavorito}>
+            {ehFavorito ? <FavoritoOnIcon /> : <FavoritoOffIcon />}
+          </button>
+        </div>
       </div>
     </div>
   )
