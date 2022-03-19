1e7cc466a536b4bb256c2a6d783e8f6a9857c5ab
Darlan Filho <nalrad.filho@gmail.com>
2022-03-19 17:46:44 -0300

    Adição do ícone de busca ao input

--- src/components/BuscaPersonagens/InputBuscaPersonagens.jsx
+++ src/components/BuscaPersonagens/InputBuscaPersonagens.jsx
@@ -3,19 +3,22 @@ import { usePersonagensControl } from 'components/ProviderPersonagens/ProviderPe
 import { makeStyles } from '@mui/styles'
 import { useFiltroContext, useFiltroControl } from 'components/ProviderFiltro/ProviderFiltroComponent'
 import { strings } from 'utils/strings'
+import { SearchIcon } from 'icons/SearchIcon'
 
 const useClasses = makeStyles({
+  root: { width: 'max(200px, 68%)', height: '3.5rem' },
+  icon: { position: 'relative', top: 'calc(50% - 20px)', left: 'calc(1rem + 30px)' },
   input: {
-    width: 'max(200px, 65%)',
-    height: '3rem',
-    padding: '0 1rem',
-    borderRadius: '1rem',
+    width: 'calc(100% - 40px)',
+    height: '100%',
+    padding: '0 1rem 0 calc(30px + 2rem)',
+    borderRadius: '2rem',
     backgroundColor: '#FDECEC',
     border: 0,
     color: '#FF0000',
     fontSize: '1rem',
     fontWeight: 600,
-    '&::placeholder': { color: '#FF0000' },
+    '&::placeholder': { color: '#FF0000A1' },
   },
 })
 
@@ -39,14 +42,19 @@ export const InputBuscaPersonagens = () => {
     if (event.key === 'Enter') personagensControl.fetchPersonagens()
   }, [])
   return (
-    <input
-      value={filtro.nameStartsWith || ''}
-      type="search"
-      data-search
-      onChange={handleChangeInput}
-      onKeyDown={handleKeyDown}
-      placeholder={strings.procurePorHerois}
-      className={classes.input}
-    />
+    <div className={classes.root}>
+      <i className={classes.icon}>
+        <SearchIcon />
+      </i>
+      <input
+        className={classes.input}
+        value={filtro.nameStartsWith || ''}
+        type="search"
+        data-search
+        onChange={handleChangeInput}
+        onKeyDown={handleKeyDown}
+        placeholder={strings.procurePorHerois}
+      />
+    </div>
   )
 }
