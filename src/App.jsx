4018aa6944ac17557bc8003ff8feb2625be3cafc
Darlan Filho <nalrad.filho@gmail.com>
2022-03-22 21:35:30 -0300

    Adição das páginas no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,21 @@ import { ProviderPersonagens } from 'components/ProviderPersonagens'
 import { ProviderFiltro } from 'components/ProviderFiltro'
 import { ProviderFavoritos } from 'components/ProviderFavoritos'
 import { filtroInicialPersonagens } from 'components/ProviderPersonagens/filtroInicialPersonagens'
-import { makeStyles } from '@mui/styles'
-import { GridPersonagens } from 'components/GridPersonagens'
-import { BuscaPersonagens } from 'components/BuscaPersonagens'
-import { HeaderPersonagens } from 'components/HeaderPersonagens'
-import { FooterPersonagens } from 'components/FooterPersonagens'
-
-const useClasses = makeStyles({
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    padding: '2rem 0 0 0',
-    gap: '3.5rem',
-    minHeight: 'calc(100vh - 2rem)',
-  },
-})
+import { ProviderPaginaPersonagem } from 'components/ProviderPaginaPersonagem'
+import { PaginaBuscaPersonagens } from 'components/PaginaBuscaPersonagens'
+import { PaginaPersonagemAtual } from 'components/PaginaPersonagemAtual'
 
 export const App = () => {
-  const classes = useClasses()
   return (
-    <div className={classes.root}>
+    <ProviderPaginaPersonagem quantidadeDeQuadrinhos={10}>
       <ProviderFavoritos chave="personagensFavoritos" limiteFavoritos={5}>
         <ProviderFiltro filtroInicial={filtroInicialPersonagens}>
           <ProviderPersonagens>
-            <HeaderPersonagens />
-            <BuscaPersonagens />
-            <GridPersonagens />
-            <FooterPersonagens />
+            <PaginaBuscaPersonagens />
+            <PaginaPersonagemAtual />
           </ProviderPersonagens>
         </ProviderFiltro>
       </ProviderFavoritos>
-    </div>
+    </ProviderPaginaPersonagem>
   )
 }
