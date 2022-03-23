114c7622d39f54ca8ace4c1ddf2c68c5b17ed23b
Darlan Filho <nalrad.filho@gmail.com>
2022-03-22 21:31:37 -0300

    Criação de componentes comuns (rating, favoritoButton e footer)

---
+++ src/components/PersonagemFavoritoIconButton/PersonagemFavoritoIconButton.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { makeStyles } from '@mui/styles'
+import { FavoritoOnIcon } from 'icons/FavoritoOnIcon'
+import { FavoritoOffIcon } from 'icons/FavoritoOffIcon'
+import { useFavoritosControl } from 'components/ProviderFavoritos/ProviderFavoritosComponent'
+
+const useClasses = makeStyles({
+  favorito: { display: 'flex', alignItems: 'center', justifyContent: 'center', marginLeft: 'auto', cursor: 'pointer' },
+  buttonInvisible: { backgroundColor: 'transparent', border: 'none', cursor: 'pointer' },
+})
+
+export const PersonagemFavoritoIconButton = ({ personagem }) => {
+  const classes = useClasses()
+  const favoritosControl = useFavoritosControl()
+  const [ehFavorito, setEhFavorito] = React.useState(favoritosControl.ehFavorito(personagem.id))
+  const handleClickFavorito = React.useCallback(() => {
+    if (ehFavorito) favoritosControl.removerFavorito(personagem.id)
+    else {
+      const favoritoAdicionado = favoritosControl.addFavorito(personagem)
+      if (!favoritoAdicionado) return console.warn('Quantidade de favoritos atingiu o limite')
+    }
+    setEhFavorito(!ehFavorito)
+  }, [ehFavorito])
+  return (
+    <div className={classes.favorito}>
+      <button className={classes.buttonInvisible} onClick={handleClickFavorito}>
+        {ehFavorito ? <FavoritoOnIcon /> : <FavoritoOffIcon />}
+      </button>
+    </div>
+  )
+}
---
+++ src/components/PersonagemFavoritoIconButton/index.js
@@ -0,0 +1,1 @@
+export { PersonagemFavoritoIconButton } from './PersonagemFavoritoIconButton'
